cb34d9073b12e600dd2135313c1a226a3a2a3c8a
Murali <muralidarreddychada@example.com>
2023-07-25 17:02:42 +0530

    logic for rendering archive

---
+++ archive.js
@@ -0,0 +1,24 @@
+import { renderContent } from "./app.js";
+
+let noteContent=JSON.parse(localStorage.getItem('notes'))||[];
+const archiveContainer=document.querySelector('.archive-notes-container');
+archiveContainer.addEventListener('click',(event)=>{
+    const type=event.target.dataset.type;
+    const id=event.target.dataset.id;
+    switch(type)
+    {
+        case "del":
+            noteContent=noteContent.filter((item)=>item.id.toString()!==id);
+            archiveContainer.innerHTML=renderContent(noteContent.filter((item)=>item.isArchive));
+            localStorage.setItem('notes',JSON.stringify(noteContent));
+            break;
+        case "archived":
+            noteContent=noteContent.map((item)=>item.id.toString()===id?{...item,isArchive:!item.isArchive}:item);
+            archiveContainer.innerHTML=renderContent(noteContent.filter((item)=>item.isArchive));
+            localStorage.setItem('notes',JSON.stringify(noteContent));
+
+
+
+    }
+})
+archiveContainer.innerHTML=renderContent(noteContent.filter((item)=>item.isArchive));
